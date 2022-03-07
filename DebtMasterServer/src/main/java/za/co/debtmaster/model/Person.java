eca46350e5b3c521bc959c0e7b4d61a593910038
mmehloma <mmehloma@student.wethinkcode.co.za>
2022-03-07 12:31:20 +0200

    Commit message: Added tests for person class

--- DebtMasterServer/src/main/java/za/co/debtmaster/model/Person.java
+++ DebtMasterServer/src/main/java/za/co/debtmaster/model/Person.java
@@ -1,4 +1,46 @@
 package za.co.debtmaster.model;
 
 public class Person {
+
+    private String name, surname, email;
+    private Budget budget;
+    private Debt debt;
+
+    public Person(String name, String surname, String email, Budget budget, Debt debt) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.budget = budget;
+        this.debt = debt;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Budget getBudget() {
+        return budget;
+    }
+
+    public Debt getDebt() {
+        return debt;
+    }
+
+    public void update(String name, String surname, String email, Budget budget, Debt debt) {
+
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.budget = budget;
+        this.debt = debt;
+    }
 }
--- DebtMasterServer/src/test/java/za/co/debtmaster/model/PersonTest.java
+++ DebtMasterServer/src/test/java/za/co/debtmaster/model/PersonTest.java
@@ -1,7 +1,35 @@
 package za.co.debtmaster.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
+
+    @Test
+    void testPerson() {
+        Budget budget = new Budget();
+        Debt debt = new Debt();
+        Person person = new Person("Mbuso", "Mehlomakulu", "mbuso@test.com", budget, debt);
+        assertEquals("Mbuso", person.getName());
+        assertEquals("Mehlomakulu", person.getSurname());
+        assertEquals("mbuso@test.com", person.getEmail());
+
+    }
+    @Test
+    void testUpdate() {
+        Budget budget = new Budget();
+        Debt debt = new Debt();
+        Person person = new Person("Mbuso", "Mehlomakulu", "mbuso@test.com", budget, debt);
+        assertEquals("Mbuso", person.getName());
+        assertEquals("Mehlomakulu", person.getSurname());
+        assertEquals("mbuso@test.com", person.getEmail());
+
+        // we call the update function
+        person.update("Seokamela", "Ntjobokoane", "david@test.com", budget, debt);
+        assertEquals("Seokamela", person.getName());
+        assertEquals("Ntjobokoane", person.getSurname());
+        assertEquals("david@test.com", person.getEmail());
+    }
 
 }
