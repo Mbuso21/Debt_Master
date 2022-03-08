e899966d14b77c8cac49e10d6825459bc973ac07
mmehloma <mmehloma@student.wethinkcode.co.za>
2022-03-08 15:51:14 +0200

    Commit message: Tried to finish Budget class

--- DebtMasterServer/src/main/java/za/co/debtmaster/model/Budget.java
+++ DebtMasterServer/src/main/java/za/co/debtmaster/model/Budget.java
@@ -1,4 +1,76 @@
 package za.co.debtmaster.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Budget {
+
+    private double totalIncome, totalExpenses;
+    private String budgetJSONString;
+
+
+    public Budget(String budgetJSONString) {
+        this.totalIncome = getNetIncome();
+        this.totalExpenses = getTotalExpenses();
+        this.budgetJSONString = budgetJSONString;
+    }
+
+    public Budget() {
+
+    }
+
+    public double getGrossIncome() {
+        System.out.println(budgetJSONString);
+        Map<String, String> requestMap = convertJSONStringToMap(budgetJSONString);
+        return Double.parseDouble(requestMap.get("income").toString());
+    }
+
+    public double getTotalExpenses() {
+        Map<String, String> requestMap = convertJSONStringToMap(budgetJSONString);
+        System.out.println(requestMap);
+        Map<String, String> expenseMap = convertJSONStringToMap(requestMap.get("expenses"));
+        double totalExpenses = 0.00;
+        for(Map.Entry<String, String> expense : expenseMap.entrySet()) {
+            totalExpenses += Double.parseDouble(expense.getValue());
+        }
+
+        return totalExpenses;
+    }
+
+    /**
+     *
+     * @return difference between income and expenses
+     */
+    public double getNetIncome() {
+
+        return totalIncome-totalExpenses;
+    }
+
+    /**
+     * Converts JSON String to Map
+     * @param budgetJSONString JSON String
+     * @return converted JSON to Map
+     */
+    public Map<String, String> convertJSONStringToMap(String budgetJSONString) {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> requestMap = null;
+        try {
+            requestMap = mapper.readValue(budgetJSONString, Map.class);
+            System.out.println(requestMap);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Can't convert to map");
+
+        }
+
+        return requestMap;
+    }
+
+
+
 }
--- DebtMasterServer/src/test/java/za/co/debtmaster/model/BudgetTest.java
+++ DebtMasterServer/src/test/java/za/co/debtmaster/model/BudgetTest.java
@@ -1,7 +1,32 @@
 package za.co.debtmaster.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BudgetTest {
+
+    public Budget budget = new Budget("{\"income\":\"15000.00\"," +
+            "\"expenses\":" +
+            "{\"rent\":\"3000.00\", " +
+            "\"groceries\":\"2000.00\", " +
+            "\"transport\":\"2000.00\"" +
+            "}" +
+            "}");
+
+    @Test
+    void testGetGrossIncome() {
+        assertEquals(15000.00, budget.getGrossIncome());
+    }
+
+    @Test
+    void testGetExpenses() {
+        assertEquals(7000.00, budget.getTotalExpenses());
+    }
+
+    @Test
+    void testIncomeAndExpenseTotals() {
+        assertEquals(5000.00, budget.getNetIncome());
+    }
 
 }
